1131fafcf05f846c387b73ef39500d9000439efb
Nick Rogers <nrogers@snf.com>
2021-08-12 19:42:03 +0000

    Remove / Refresh Code Base

--- resources/views/partials/footer.blade.php
+++
@@ -1,9 +0,0 @@
-<footer class="main-footer">
-    <strong>Copyright © <script>
-            document.write(new Date().getFullYear())
-        </script> Nick Rogers.</strong>
-    All rights reserved.
-    <div class="float-right d-none d-sm-inline-block">
-        <b>Version</b> 1.0.0
-    </div>
-</footer>
